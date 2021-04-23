4db53a488a524e6308e815541892d01318d6253e
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2021-04-23 12:02:18 +0200

    feat responsive

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
-
-
 function toogleMenu(navId){
   document.getElementById(navId).classList.toggle("show-nav");
+}
+
+function hideMenu(navId){
+  document.getElementById(navId).classList.remove("show-nav");
 }
