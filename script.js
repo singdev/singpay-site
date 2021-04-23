4d1b3894c03f469ec1c82c8e60771c3ace86ea21
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2021-04-23 15:01:11 +0200

    feat contact send message

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ function toogleMenu(navId){
 function hideMenu(navId){
   document.getElementById(navId).classList.remove("show-nav");
 }
+
+async function sendMessage(){
+  const nom = document.getElementById("nom").value;
+  const email = document.getElementById("email").value;
+  const telephone = document.getElementById("telephone").value;
+  const message = document.getElementById("message").value;
+  
+  const htmlContent = getMessage(nom, email, telephone, message);
+  const dest = `norpheehounkponou@gmail.com`;
+  const subject = "Nouveau message";
+  const labelName = 'SingPay';
+  
+  const url = "http://localhost:3000/mail";
+  
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      htmlContent, dest, subject, labelName
+    })
+  });
+  
+  if(res.status == 200){
+    alert("Message envoyé avec succès");
+  } else {
+    const message = await res.json();
+    console.log(message);
+    alert("Votre message n'a pas été envoyé, veullez recommancer s'il vous plait");
+  }
+}
