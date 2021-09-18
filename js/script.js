69e167509a3f5a2b39cc2383609db84f6ebccbed
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2021-09-18 22:50:30 +0200

    feat update

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ function hideMenu(navId){
 }
 
 async function sendMessage(){
+  const feeadback = document.getElementById("feedback");
+  feeadback.innerHTML = "Veuillez patientez !";
+  
   const nom = document.getElementById("nom").value;
   const email = document.getElementById("email").value;
   const telephone = document.getElementById("telephone").value;
@@ -17,23 +20,32 @@ async function sendMessage(){
   const subject = "Nouveau message";
   const labelName = 'SingPay';
   
-  const url = "https://client.singpay.ga/mail";
+  try {
+    const url = "https://client.singpay.ga/mail";
   
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type":"application/json"
-    },
-    body: JSON.stringify({
-      htmlContent, dest, subject, labelName
-    })
-  });
-  
-  if(res.status == 200){
-    alert("Message envoyé avec succès");
-  } else {
-    const message = await res.json();
-    console.log(message);
-    alert("Votre message n'a pas été envoyé, veullez recommancer s'il vous plait");
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        htmlContent, dest, subject, labelName
+      })
+    });
+    
+    if(res.status == 200){
+      alert("Message envoyé avec succès");
+      feeadback.innerHTML = "Message envoyé avec succès";
+      feeadback.style.color = "green";
+    } else {
+      const message = await res.json();
+      console.log(message);
+      alert("Votre message n'a pas été envoyé, veullez recommancer s'il vous plait");
+      feeadback.innerHTML = "Votre message n'a pas été envoyé, veullez recommancer s'il vous plait";
+      feeadback.style.color = "red";
+    }
+  } catch(err){
+    console.log(err);
+    feeadback.innerHTML = "";
   }
 }
